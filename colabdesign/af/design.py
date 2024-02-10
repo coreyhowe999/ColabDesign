19cd67d1fa9d32c5461e0178e003cd37b6586ef2
coreyhowe9 <37548364+coreyhowe999@users.noreply.github.com>
2024-02-09 23:11:22 -0600

    Update design.py

--- colabdesign/af/design.py
+++ colabdesign/af/design.py
@@ -535,7 +535,7 @@ class _af_design:
         aux = self.predict(seq=mut_seq, return_aux=True, model_nums=model_nums, verbose=False, **kwargs)
         buff.append({"aux":aux, "seq":np.array(mut_seq)})
         current_loss = aux["loss"]
-        print('best loss:',pre_loss,'candidate:',current_loss)
+        print('best loss:',prev_loss,'candidate:',current_loss)
         
       print('num tries to improvement:',num_tries)
       print('num residues tried:',aa_try_idx+1)
