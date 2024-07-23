38fa9c4e960cf8f3e64871794773e7902f10b138
coreyhowe9 <37548364+coreyhowe999@users.noreply.github.com>
2024-07-23 11:00:13 -0600

    Update __init__.py

--- colabdesign/__init__.py
+++ colabdesign/__init__.py
@@ -6,7 +6,7 @@ if int(jax.__version__.split(".")[1]) > 3:
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-from colabdesign.shared.utils import clear_mem
+from colabdesign.shared.utils import clear_mem, add_cyclic_offset
 from colabdesign.af.model import mk_af_model
 from colabdesign.tr.model import mk_tr_model
 from colabdesign.mpnn.model import mk_mpnn_model
